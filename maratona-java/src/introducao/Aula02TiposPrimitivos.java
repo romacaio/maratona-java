4d989b606af2f4214b28ba7d361e9fa483ff012c
Caio Romão <caiodev14@gmail.com>
2025-11-17 11:24:10 -0300

    feature: Aula03 - Casting

--- maratona-java/src/introducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/introducao/Aula02TiposPrimitivos.java
@@ -5,7 +5,15 @@ public class Aula02TiposPrimitivos {
 
         // int, double, float, char, byte, long, boolean
 
+        byte idadeByte = (byte) 1000;
+        short idadeShort = (short) 100000; //casting explícito
         int idade = 10;
-        System.out.println("a idade é " + idade + " anos.");
+        long numeroGrande = idade; //casting implícito
+        float salarioFloat = 2500.32F;
+        double salarioDouble = 2000;
+        char caracter = 'A';
+        boolean isVerdade = true;
+        boolean isFalso = false;
+        System.out.println("a idade é " + numeroGrande + " anos.");
     }
 }
