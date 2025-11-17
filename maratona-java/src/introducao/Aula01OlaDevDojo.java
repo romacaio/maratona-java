d09706f08a5010e0f4241a6e62ae19d778a598fb
Caio Romão <caiodev14@gmail.com>
2025-11-17 09:38:18 -0300

    feature: Aula01 - Primeiro programa e organizando pacotes

---
+++ maratona-java/src/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,15 @@
+package introducao;
+
+public class Aula01OlaDevDojo {
+    // isso é um comentário de linha única
+
+    /*
+        isso é um comentário
+        de múltiplas
+        linhas
+     */
+    public static void main (String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+
+    }
+}
