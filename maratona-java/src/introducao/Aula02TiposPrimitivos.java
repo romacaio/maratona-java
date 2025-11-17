958b1737e8c3043aa0426b5fa5cc873939c1aa4e
Caio Romão <caiodev14@gmail.com>
2025-11-17 09:58:19 -0300

    feature: Aula02 - Tipos primitivos pt01

---
+++ maratona-java/src/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        // int, double, float, char, byte, long, boolean
+
+        int idade = 10;
+        System.out.println("a idade é " + idade + " anos.");
+    }
+}
