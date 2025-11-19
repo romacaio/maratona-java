4d1f1c33a38635c230a9582d01ec2bf3d06e4c85
Caio Romão <caiodev14@gmail.com>
2025-11-19 09:17:02 -0300

    feature: aula10 - operadores atribuição

--- maratona-java/src/introducao/Aula04Operadores.java
+++ maratona-java/src/introducao/Aula04Operadores.java
@@ -53,5 +53,25 @@ public class Aula04Operadores {
 
         System.out.println("isValorPlaystationCincoCompravel " + isValorPlaystationCincoCompravel);
 
+        // += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;  // bonus = bonus + 1000;
+        bonus -= 1000;
+        bonus *=2;
+        bonus /=2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // ++ --
+
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador ++; // 2
+        contador --; // 1
+        int contador2 = 0;
+        System.out.println(contador);
+        System.out.println(contador2++);
+        System.out.println(contador2);
+
     }
 }
