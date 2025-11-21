985ce41c0e3b0cdfc54548c67805cd002a29fdcb
Caio Romão <caiodev14@gmail.com>
2025-11-21 09:45:23 -0300

    feature: Aula15 - Estruturas condicionais pt05 Switch

---
+++ maratona-java/src/introducao/struturasCondicionais05.java
@@ -0,0 +1,53 @@
+package introducao;
+
+public class struturasCondicionais05 {
+    public static void main(String[] args) {
+        // imprima o dia da semana considerando 1 como domingo
+        byte dia = 6;
+        // aceita: char, int, byte, short, enum, String
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+
+            case 2:
+                System.out.println("Segunda");
+                break;
+
+            case 3:
+                System.out.println("Terça");
+                break;
+
+            case 4:
+                System.out.println("Quarta");
+                break;
+
+            case 5:
+                System.out.println("Quinta");
+                break;
+
+            case 6:
+                System.out.println("Sexta");
+                break;
+
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println();
+        }
+
+        char sexo = 'M';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+            default:
+                System.out.println("Sexo não identificado");
+                break;
+        }
+    }
+}
