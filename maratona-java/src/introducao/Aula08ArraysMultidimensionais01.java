eb6f7e1b9843fe3eda9db2ac99bc66e9e1f19477
Caio Romão <caiodev14@gmail.com>
2025-12-04 13:23:06 -0300

    feature: Aula27 - Arrays Multidimensionais pt02 Foreach

--- maratona-java/src/introducao/Aula08ArraysMultidimensionais01.java
+++ maratona-java/src/introducao/Aula08ArraysMultidimensionais01.java
@@ -17,9 +17,16 @@ public class Aula08ArraysMultidimensionais01 {
 
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
 
+            }
+        }
+        System.out.println("---------------------");
+
+        for (int[] arrayBase : dias) {
+            for(int num : arrayBase){
+                System.out.println(num);
             }
         }
     }
