fbd0fc79fe62e1fb93c2ccb1bc9102c85d1e3c77
Caio Romão <caiodev14@gmail.com>
2025-12-04 09:54:40 -0300

    feature: Aula24 - Arrays pt03

---
+++ maratona-java/src/introducao/Aula07Arrays03.java
@@ -0,0 +1,15 @@
+package introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+
+        String[] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+
+        for (int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
+    }
+}
