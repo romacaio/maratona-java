59e2b32d347e5be433243a63a6c3f82c72684dbe
Caio Romão <caiodev14@gmail.com>
2025-12-05 14:13:48 -0300

    feature: Aula29 - Orientação Objetos pt01 Introdução classes

---
+++ maratona-java/src/javacore/introduçaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package javacore.introduçaoclasses.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ maratona-java/src/javacore/introduçaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,18 @@
+package javacore.introduçaoclasses.test;
+
+import javacore.introduçaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+
+        Estudante estudante = new Estudante();
+        estudante.nome = "luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+
+}
