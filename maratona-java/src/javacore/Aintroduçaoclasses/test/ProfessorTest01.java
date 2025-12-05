397e04586e67b5f6e23c51cfa4b64833a7e32b2f
Caio Romão <caiodev14@gmail.com>
2025-12-05 15:00:13 -0300

    feature: Aula31 - Orientação objetos Coesão

---
+++ maratona-java/src/javacore/Aintroduçaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,14 @@
+package javacore.Aintroduçaoclasses.test;
+
+import javacore.Aintroduçaoclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+
+        Professor professor = new Professor();
+        professor.nome = "Mestre Kami";
+        professor.idade = 140;
+        professor.sexo = 'M';
+        System.out.println("Nome: " + professor.nome + " Idade: " + professor.idade + " Sexo: " + professor.sexo);
+    }
+}
