079701d843af678417f914e3db625dd72241dd5f
Caio Romão <caiodev14@gmail.com>
2025-12-06 18:57:36 -0300

    feature: Aula35 - Orientação objetos métodos pt02 Parâmentros

---
+++ maratona-java/src/javacore/Bintroduçaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package javacore.Bintroduçaometodos.test;
+
+import javacore.Bintroduçaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicarDoisNumeros(10,5);
+    }
+}
