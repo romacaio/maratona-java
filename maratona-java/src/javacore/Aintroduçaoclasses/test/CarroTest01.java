0b39216713ee3c0aae7f1315bb278b9109700382
Caio Romão <caiodev14@gmail.com>
2025-12-06 09:53:42 -0300

    feature: Aula33 - Orientação objetos Referência de objetos

--- maratona-java/src/javacore/Aintroduçaoclasses/test/CarroTest01.java
+++ maratona-java/src/javacore/Aintroduçaoclasses/test/CarroTest01.java
@@ -16,6 +16,8 @@ public class CarroTest01 {
         carro2.modelo = "Gol BX";
         carro2.ano = 1980;
 
+        carro1 = carro2;
+
         System.out.println("Carro 1 " + "\nNome: " + carro1.nome + "\nModelo: " + carro1.modelo + "\nAno: " + carro1.ano);
         System.out.println();
         System.out.println("Carro 2 " + "\nNome: " + carro2.nome + "\nModelo: " + carro2.modelo + "\nAno: " + carro2.ano);
