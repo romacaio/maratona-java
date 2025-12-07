16bac282a1985e6e69ba32316b26d39cf219578e
Caio Romão <caiodev14@gmail.com>
2025-12-06 21:37:28 -0300

    feature: Aula39 - Orientação objetos métodos pt06 Parâmetros tipo referência

---
+++ maratona-java/src/javacore/Bintroduçaometodos/test/EstudanteTest01.java
@@ -0,0 +1,22 @@
+package javacore.Bintroduçaometodos.test;
+
+import javacore.Bintroduçaometodos.dominio.Estudante;
+import javacore.Bintroduçaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        estudante01.nome = "Midorya";
+        estudante01.sexo = 'M';
+        estudante01.idade = 15;
+        Estudante estudante02 = new Estudante();
+        estudante02.nome = "Sakura";
+        estudante02.sexo = 'F';
+        estudante02.idade = 16;
+
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+        impressora.imprime(estudante01);
+        System.out.println("--------------");
+        impressora.imprime(estudante02);
+    }
+}
