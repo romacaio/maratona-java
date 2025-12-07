0c0a4a6e9043123da8fa5967e2a6b63452c20d20
Caio Romão <caiodev14@gmail.com>
2025-12-07 10:36:03 -0300

    feature: Aula41 - Orientação objetos métodos pt08 Referência this

--- maratona-java/src/javacore/Bintroduçaometodos/test/EstudanteTest01.java
+++ maratona-java/src/javacore/Bintroduçaometodos/test/EstudanteTest01.java
@@ -5,10 +5,12 @@ import javacore.Bintroduçaometodos.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
+
         Estudante estudante01 = new Estudante();
         estudante01.nome = "Midorya";
         estudante01.sexo = 'M';
         estudante01.idade = 15;
+
         Estudante estudante02 = new Estudante();
         estudante02.nome = "Sakura";
         estudante02.sexo = 'F';
