cd63bd72f39e0d3322a02161e1f353126eb03bb7
Caio Romão <caiodev14@gmail.com>
2025-12-07 10:20:27 -0300

    feature: Aula40 - Orientação objetos métodos pt07 Parâmetros tipo referência pt02

--- maratona-java/src/javacore/Bintroduçaometodos/test/EstudanteTest01.java
+++ maratona-java/src/javacore/Bintroduçaometodos/test/EstudanteTest01.java
@@ -18,5 +18,10 @@ public class EstudanteTest01 {
         impressora.imprime(estudante01);
         System.out.println("--------------");
         impressora.imprime(estudante02);
+
+        System.out.println("######################");
+        impressora.imprime(estudante01);
+        System.out.println("--------------");
+        impressora.imprime(estudante02);
     }
 }
