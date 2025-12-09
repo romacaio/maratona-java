2f31e3cb902e621ab39842008b2f0a3ef9dc4a1a
Caio Romão <caiodev14@gmail.com>
2025-12-08 21:11:20 -0300

    feature: Aula47 - Orientação objetos Sobrecarga de métodos

---
+++ maratona-java/src/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package javacore.Csobrecargametodos.test;
+
+import javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime();
+        anime.init("Akudama Drive", "Tv", 12, "Ação");
+        anime.imprime();
+    }
+
+}
