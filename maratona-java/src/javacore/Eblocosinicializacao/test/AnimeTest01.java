42c783020b9a729bd29c2861e7131dd717369e2f
Caio Romão <caiodev14@gmail.com>
2025-12-09 14:56:30 -0300

    feature: Aula50 - 0rientação objetos Blocos de incialização

---
+++ maratona-java/src/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -0,0 +1,15 @@
+package javacore.Eblocosinicializacao.test;
+
+import javacore.Eblocosinicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime("One Piece");
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+
+        }
+
+    }
+}
