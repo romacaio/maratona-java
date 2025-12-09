d641c36626a3cdc530290ca485cacc77ae60369b
Caio Romão <caiodev14@gmail.com>
2025-12-09 20:28:20 -0300

    feature: Aula51 - Orientação objetos Modificador static

---
+++ maratona-java/src/javacore/Fmodificadorestatico/dominio/Carro.java
@@ -0,0 +1,36 @@
+package javacore.Fmodificadorestatico.dominio;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250;
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public void imprime() {
+        System.out.println("-------------");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("velocidadeMaxima: " + this.velocidadeMaxima);
+        System.out.println("velocidadeLimite: " + Carro.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return this.velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+}
