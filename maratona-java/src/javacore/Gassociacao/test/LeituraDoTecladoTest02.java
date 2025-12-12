3fbec983f6a5dc557c0736584b9651becc5cff9b
Caio Romão <caiodev14@gmail.com>
2025-12-12 14:27:45 -0300

    feature: Aula59 - Orientação objetos Leitura de dados pelo console exercício

---
+++ maratona-java/src/javacore/Gassociacao/test/LeituraDoTecladoTest02.java
@@ -0,0 +1,20 @@
+package javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("O grande Software de previsão do futuro");
+        System.out.println("Digite sua pergunta e eu responderei sim ou não");
+        String pergunta = sc.nextLine();
+
+        if (pergunta.charAt(0) == ' ') {
+            System.out.println("SIM");
+        } else {
+            System.out.println("NÃO");
+        }
+
+    }
+}
