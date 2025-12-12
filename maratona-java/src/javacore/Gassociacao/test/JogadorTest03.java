f8f6f4eb86babc0d3052cd1998200d66bab524c2
Caio Romão <caiodev14@gmail.com>
2025-12-12 10:46:00 -0300

    feature: Aula57 - Orientação objetos Associação pt04 associação bidirecional

---
+++ maratona-java/src/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,24 @@
+package javacore.Gassociacao.test;
+
+import javacore.Gassociacao.dominio.Jogador;
+import javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador1,jogador2};
+
+        jogador1.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("--- Jogador ---");
+        jogador1.imprime();
+        jogador2.imprime();
+        System.out.println("--- Time ---");
+        time.imprime();
+
+    }
+}
