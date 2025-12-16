0d8f3eb1457e90ff0a165b24d4437e4ae3c639f8
Caio Romão <caiodev14@gmail.com>
2025-12-15 21:40:45 -0300

    feature: Aula67 - Orientação objetos Modificador final pt01 Tipos primitivos

---
+++ maratona-java/src/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,14 @@
+package javacore.Jmodificadorfinal.dominio;
+
+public class Carro {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
