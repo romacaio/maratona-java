dce3c5eea294ccf6e7900cc24d65f0b7b1a92860
Caio Romão <caiodev14@gmail.com>
2025-12-19 19:22:55 -0300

    feature: Aula75 - Orientação objetos Classes abstratas pt02 métodos abstratos

--- maratona-java/src/javacore/Lclassesabstratas/dominio/Desenvolvedor.java
+++ maratona-java/src/javacore/Lclassesabstratas/dominio/Desenvolvedor.java
@@ -5,4 +5,9 @@ public class Desenvolvedor extends Funcionario {
     public Desenvolvedor(String nome, double salario) {
         super(nome, salario);
     }
+
+    @Override
+    public void calculaBonus() {
+        this.salario = salario + salario * 0.05;
+    }
 }
--- maratona-java/src/javacore/Lclassesabstratas/dominio/Gerente.java
+++ maratona-java/src/javacore/Lclassesabstratas/dominio/Gerente.java
@@ -7,6 +7,11 @@ public class Gerente extends Funcionario {
     }
 
     @Override
+    public void calculaBonus() {
+        this.salario = salario + salario * 0.2;
+    }
+
+    @Override
     public String toString() {
         return "Gerente - Nome: " + this.nome + ", " + "Salário " + this.salario;
     }
