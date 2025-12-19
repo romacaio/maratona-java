2b607c96df99f969a6af8b65fdd094dc1412628a
Caio Romão <caiodev14@gmail.com>
2025-12-19 10:00:53 -0300

    feature: Aula73 - Orientação objetos Enumeração pt04 Busca por atributos

--- maratona-java/src/javacore/Kenum/dominio/TipoCliente.java
+++ maratona-java/src/javacore/Kenum/dominio/TipoCliente.java
@@ -4,11 +4,20 @@ public enum TipoCliente {
     PESSOA_FISICA(1, "Pessoa Física"),
     PESSOA_JURIDICA(2, "Pessoa Jurídica");
     public final int VALOR;
-    private String nomeRelatiorio;
+    private final String nomeRelatiorio;
 
     private TipoCliente(int VALOR, String nomeRelatiorio) {
         this.VALOR = VALOR;
         this.nomeRelatiorio = nomeRelatiorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatiorio0) {
+        for (TipoCliente tipo : values()) {
+            if (tipo.nomeRelatiorio.equals(nomeRelatiorio0)) {
+                return tipo;
+            }
+        }
+        return null;
     }
 
     public String getNomeRelatiorio() {
--- maratona-java/src/javacore/Kenum/test/Cliente01.java
+++ maratona-java/src/javacore/Kenum/test/Cliente01.java
@@ -13,5 +13,13 @@ public class Cliente01 {
         System.out.println(cliente2);
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+
+        TipoCliente tipoCliente1 = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente1.getNomeRelatiorio());
+
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+        System.out.println(tipoCliente2);
+
+
     }
 }
