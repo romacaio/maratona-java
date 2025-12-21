b0e6e6be5e08ee6b31edfd461bcb5043857b46b1
Caio Romão <caiodev14@gmail.com>
2025-12-21 16:27:27 -0300

    feature: Aula83 - Orientação objetos Polimorfismo pt04 cast e instanceof

--- maratona-java/src/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ maratona-java/src/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,6 +1,9 @@
 package javacore.Npolimorfismo.servico;
 
+import javacore.Isobrescrita.dominio.Anime;
+import javacore.Npolimorfismo.dominio.Computador;
 import javacore.Npolimorfismo.dominio.Produto;
+import javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
 
@@ -10,5 +13,9 @@ public class CalculadoraImposto {
         System.out.println("Produto: " + produto.getNome());
         System.out.println("Valor: " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
+        if (produto instanceof Tomate) {
+            Tomate tomate = (Tomate) produto;
+            System.out.println("Data de validade: " + tomate.getDataValidade());
+        }
     }
 }
