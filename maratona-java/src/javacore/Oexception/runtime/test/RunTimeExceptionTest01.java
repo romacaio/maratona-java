770188d8f4c5e74e0caeb835c746200abadd5d97
Caio Romão <caiodev14@gmail.com>
2025-12-29 21:59:54 -0300

    feature: Aula88 - Orientação objetos pt04 - Lançando exceção unchecked

--- maratona-java/src/javacore/Oexception/runtime/test/RunTimeExceptionTest01.java
+++ maratona-java/src/javacore/Oexception/runtime/test/RunTimeExceptionTest01.java
@@ -3,10 +3,10 @@ package javacore.Oexception.runtime.test;
 public class RunTimeExceptionTest01 {
     public static void main(String[] args) {
 
-//        Object obj = null;
-//        System.out.println(obj.toString());
+        // Object obj = null;
+        // System.out.println(obj.toString());
 
-        int[] array = {1,2};
+        int[] array = {1, 2};
         System.out.println(array[2]);
     }
 }
---
+++ maratona-java/src/javacore/Oexception/runtime/test/RunTimeExceptionTest02.java
@@ -0,0 +1,28 @@
+package javacore.Oexception.runtime.test;
+
+public class RunTimeExceptionTest02 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(divisao(1, 0));
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Código finalizado");
+    }
+
+    /**
+     *
+     * @param a
+     * @param b não pode ser zero
+     * @return
+     * @throws IllegalArgumentException caso b seja 0
+     */
+
+    public static int divisao(int a, int b) {
+
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento invalido, não pode ser 0");
+        }
+        return a / b;
+    }
+}
