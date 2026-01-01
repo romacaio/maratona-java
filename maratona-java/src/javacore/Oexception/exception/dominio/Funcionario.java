51bd6ad1a234d8d62d2b305b9373a1bc62cdddf3
Caio Romão <caiodev14@gmail.com>
2026-01-01 15:07:20 -0300

    feature: Aula95 - Orientação objetos Exceções pt11 Exceções e regras de sobrescrita

---
+++ maratona-java/src/javacore/Oexception/exception/dominio/Funcionario.java
@@ -0,0 +1,12 @@
+package javacore.Oexception.exception.dominio;
+
+
+import java.io.FileNotFoundException;
+
+public class Funcionario extends Pessoa {
+
+    @Override
+    public void salvar() throws LoginInvalidoException, FileNotFoundException {
+        System.out.println("Salvando funcionário");
+    }
+}
