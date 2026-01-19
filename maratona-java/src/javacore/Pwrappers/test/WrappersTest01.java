baa903267ebdb9d9cdd1e200c9b04bae609c0619
Caio Romão <caiodev14@gmail.com>
2026-01-19 10:33:46 -0300

    feature: Aula96 Classes utilitárias - Wrappers pt01

---
+++ maratona-java/src/javacore/Pwrappers/test/WrappersTest01.java
@@ -0,0 +1,24 @@
+package javacore.Pwrappers.test;
+
+public class WrappersTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10;
+        char charP = 'A';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floaWt = 10F;
+        Double doubleW = 10D;
+        Character charW = 'A';
+        Boolean booleanW = false;
+
+    }
+}
