af2351778135d343c490e6986a08b97b56ac634f
Caio Romão <caiodev14@gmail.com>
2026-01-19 20:42:00 -0300

    feature: Aula102 Classes utilitárias - Date

---
+++ maratona-java/src/javacore/Rdatas/DateTest01.java
@@ -0,0 +1,13 @@
+package javacore.Rdatas;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+
+        Date date = new Date(1768865006595L); // long
+        System.out.println(date.getTime());
+        date.setTime(date.getTime() + 3_600_000); // adiciona 1 hora
+        System.out.println(date);
+    }
+}
