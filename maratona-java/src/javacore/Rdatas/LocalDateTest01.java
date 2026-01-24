5eac720c00353ada9e01f1b54e333727d4dead52
Caio Romão <caiodev14@gmail.com>
2026-01-24 14:09:48 -0300

    feature: Aula109 classes utilitarias - LocalDate

---
+++ maratona-java/src/javacore/Rdatas/LocalDateTest01.java
@@ -0,0 +1,31 @@
+package javacore.Rdatas;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+
+        LocalDate date = LocalDate.of(2025, Month.JANUARY, 24);
+        LocalDate agora = LocalDate.now();
+
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getMonthValue());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.lengthOfMonth());
+        System.out.println(date.isLeapYear()); // é ano bissexto?
+        System.out.println(date.get(ChronoField.YEAR_OF_ERA));  // campos mais específicos
+
+        System.out.println(date);
+        System.out.println(agora);
+
+    }
+}
