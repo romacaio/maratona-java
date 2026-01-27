cd75c9a14649674deda289c669662905004df6d2
Caio Romão <caiodev14@gmail.com>
2026-01-27 10:00:43 -0300

    feature: Aula118 - Classes utilitárias - ZonedDateTime, ZoneID, ZoneOffSet, OffSetDateTime

---
+++ maratona-java/src/javacore/Rdatas/ZoneTest01.java
@@ -0,0 +1,51 @@
+package javacore.Rdatas;
+
+import java.time.*;
+import java.time.chrono.JapaneseDate;
+import java.util.Map;
+
+public class ZoneTest01 {
+    public static void main(String[] args) {
+
+        Map<String, String> shortIds = ZoneId.SHORT_IDS;
+        System.out.println(shortIds);
+
+        System.out.println(ZoneId.systemDefault());
+
+        ZoneId tokyoZone = ZoneId.of("Asia/Tokyo");
+        System.out.println(tokyoZone);
+
+        LocalDateTime now = LocalDateTime.now();
+        ZonedDateTime zonedDateTime1 = now.atZone(tokyoZone);
+        System.out.println(zonedDateTime1);
+
+        Instant nowInstant = Instant.now();
+        System.out.println(nowInstant);
+        ZonedDateTime zonedDateTime2 = nowInstant.atZone(tokyoZone); // converte o horário e imprime o ZoneId
+        System.out.println(zonedDateTime2);
+
+        LocalDateTime nowTokyo = LocalDateTime.now(tokyoZone); // converte o horário, mas não imprime o ZoneId
+        System.out.println(nowTokyo);
+
+        System.out.println(ZoneOffset.MAX);
+        System.out.println(ZoneOffset.MIN);
+
+        ZoneOffset offSetManaus = ZoneOffset.of("-04:00");
+        OffsetDateTime offsetDateTime1 = now.atOffset(offSetManaus);
+        System.out.println(offsetDateTime1);
+
+        OffsetDateTime offsetDateTime2 = OffsetDateTime.of(now, offSetManaus);
+        System.out.println(offsetDateTime2);
+
+        OffsetDateTime offsetDateTime3 = nowInstant.atOffset(offSetManaus);
+        System.out.println(offsetDateTime3);
+
+        JapaneseDate japaneseDate = JapaneseDate.from(LocalDate.now());
+        System.out.println(japaneseDate);
+        LocalDate meijiEraLocalDate = LocalDate.of(1900,2,1);
+        JapaneseDate meijiEra = JapaneseDate.from(meijiEraLocalDate);
+        System.out.println(meijiEra);
+
+
+    }
+}
