abb03e000b80b72cf5c63a4b7ea314599769948e
Caio Romão <caiodev14@gmail.com>
2026-02-06 10:35:46 -0300

    feature: Aula128 Classes utilitárias IO pt02 - FileWriter

---
+++ maratona-java/src/javacore/vio/test/FileWriterTest01.java
@@ -0,0 +1,26 @@
+package javacore.vio.test;
+
+// File
+// FileWriter
+// FileReader
+// BufferedWriter
+// BufferReader
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest01 {
+    public static void main(String[] args) {
+
+        File file = new File("file.txt");
+
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write("O Dev Dojo é lindo, é o melhor curso do brasilllll\nContinuando a cantoria na próxima linha\n");
+            fw.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
