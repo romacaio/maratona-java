4d31ef7395174c91af2c86d4c4bb6089cc300a8a
Caio Romão <caiodev14@gmail.com>
2026-03-12 11:03:49 -0300

    feature: Aula155 Coleções - List pt02

--- maratona-java/src/javacore/Ycolecoes/ListTest01.java
+++ maratona-java/src/javacore/Ycolecoes/ListTest01.java
@@ -11,12 +11,12 @@ public class ListTest01 {
         várias tipos de objetos...
          */
 
-        List nomes = new ArrayList();
-        nomes.add("William");
-        nomes.add("DevDojo Academy");
-        nomes.add(123);
+        List lista = new ArrayList();
+        lista.add("William");
+        lista.add("DevDojo Academy");
+        lista.add(123);
 
-        for (Object nome : nomes) {
+        for (Object nome : lista) {
             System.out.println(nome);
         }
         System.out.println("-".repeat(20));
@@ -25,19 +25,36 @@ public class ListTest01 {
         Forma atual, since: 1.5 -> Utiliza genérics para definir o tipo objeto e força em tempo de compilação
          */
 
-        List<String> nomes2 = new ArrayList<>();
-        nomes2.add("Caio");
-        nomes2.add("luiz");
+        List<String> nomes1 = new ArrayList<>();
+        nomes1.add("Caio");
+        nomes1.add("luiz");
+        System.out.println(nomes1.remove(1));
 
-        for (String nome : nomes2) {
+        for (String nome : nomes1) {
             System.out.println(nome);
         }
         System.out.println();
 
-        nomes2.add("Jeff");
-        for (int i = 0; i < nomes2.size(); i++) {
-            System.out.println(nomes2.get(i));
+        nomes1.add("Jeff");
+        for (int i = 0; i < nomes1.size(); i++) {
+            System.out.println(nomes1.get(i));
         }
 
+        List<String> nomes2 = new ArrayList<>();
+        nomes2.add("Ana");
+        nomes2.add("Julia");
+
+        nomes1.addAll(nomes2);
+        System.out.println(nomes1);
+
+        /*
+        Coleções não permitem tipos primitivos, pois internamente utilizam equals e hashCode e por não
+        serem objetos não possuem.
+
+        Utilizamos os Wrappers para representar os tipos primitivos.
+         */
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }
