7354985027488f9c84843a33d972ecb19a195d7e
Caio Romão <caiodev14@gmail.com>
2026-03-19 11:18:29 -0300

    feature: Aula165 Coleções - NavigableSet, TreeSet pt02

--- maratona-java/src/javacore/Ycolecoes/test/NavigableSetTest01.java
+++ maratona-java/src/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -14,26 +14,59 @@ class SmartphoneMarcaComparator implements Comparator<Smartphone> {
     }
 }
 
+class MangaPrecoComparator implements Comparator<Manga> {
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return Double.compare(manga1.getPreco(), manga2.getPreco());
+    }
+}
+
 public class NavigableSetTest01 {
 
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
-        System.out.println(smartphone);
+        System.out.println(smartphone + "\n");
 
-        NavigableSet<Manga> mangas = new TreeSet<>();
-
-        mangas.add(new Manga(1L, "Hellsing Ultimate", 9.5, 0));
-        mangas.add(new Manga(5L, "Bersek", 19.9, 5));
-        mangas.add(new Manga(3L, "Pokemon", 11.20, 0));
-        mangas.add(new Manga(4L, "Attack on Titan", 3.2, 2));
-        mangas.add(new Manga(2L, "Dragon Ball Z", 2.99, 0));
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
+        mangas.add(new Manga(1L, "Hellsing Ultimate", 19.9, 0));
+        mangas.add(new Manga(5L, "Bersek", 9.5, 5));
+        mangas.add(new Manga(3L, "Pokemon", 3.2, 0));
+        mangas.add(new Manga(4L, "Attack on Titan", 11.20, 2));
         mangas.add(new Manga(2L, "Dragon Ball Z", 2.99, 0));
         mangas.add(new Manga(10L, "Aaragon", 2.99, 0));
 
-        for(Manga manga : mangas){
+        for (Manga manga : mangas) {
             System.out.println(manga);
         }
+        System.out.println("-".repeat(50));
+
+        Manga yuyu = new Manga(21L, "Yuyu Hakusho", 3.2, 5);
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println("-".repeat(50));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.size());
+
+        /* métodos do Navigable:
+
+        descendingSet() -> começa de baixo pra cima da árvore (ordem inversa).
+        
+        lower <
+        floor <=
+        higher >
+        ceiling >=
+
+        pollFirst -> remove e retorna o menor elemento (no caso o primeiro)
+        poolLast -> remove e retorna o maio elemento (no caso o último)
+         */
+
     }
 }
+
