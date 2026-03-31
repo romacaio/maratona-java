b4d78b4b173b415a2a8f91550a7a0dd5f29ac201
Caio Romão <caiodev14@gmail.com>
2026-03-31 19:53:51 -0300

    feature: aula183 - Lambdas pt01 Predicate

--- maratona-java/src/javacore/ZZBcomportamentos/interfaces/CarroPredicate.java
+++ maratona-java/src/javacore/ZZBcomportamentos/interfaces/CarroPredicate.java
@@ -2,7 +2,19 @@ package javacore.ZZBcomportamentos.interfaces;
 
 import javacore.ZZBcomportamentos.dominio.Carro;
 
+@FunctionalInterface
 public interface CarroPredicate {
 
+    // Lambdas: são anonimas, funções, deixar o código mais conciso
     public abstract boolean test(Carro carro);
+
+    /*
+    (parametro) -> (expressão)
+    (Carro carro) -> (carro.getCor().equals("verde"));
+
+    forma abreviada:
+    carro -> carro.getCor().equals("verde")
+
+ */
+
 }
