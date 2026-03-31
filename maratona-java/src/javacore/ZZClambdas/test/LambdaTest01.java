b738fba723227734a1d9a9b0090ad5205bf89249
Caio Romão <caiodev14@gmail.com>
2026-03-31 20:40:20 -0300

    feature: aula184 - Lambdas pt02 Consumer

---
+++ maratona-java/src/javacore/ZZClambdas/test/LambdaTest01.java
@@ -0,0 +1,26 @@
+package javacore.ZZClambdas.test;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LambdaTest01 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("William", "Suane", "Luffy", "Zoro");
+        forEach(strings, nome -> System.out.println(nome));
+
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7);
+        forEach(integers, num -> System.out.println(num));
+    }
+
+    private static <T> void forEach(List<T> lista, Consumer<T> consumer) {
+        for (T e : lista) {
+            consumer.accept(e);
+
+            /*
+            public abstract <T> void accept(T t) -> basicamente o predicate mas com retorno void.
+            serve para executar uma ação: (imprimir, salvar, enviar)
+             */
+
+        }
+    }
+}
