4364d804d3150f0effbd782a73f0eb4ca0f1c8a4
Caio Romão <caiodev14@gmail.com>
2026-04-06 11:21:41 -0300

    feature: aula189 - Optional pt01

---
+++ maratona-java/src/javacore/ZZDoptional/test/OptionalTest01.java
@@ -0,0 +1,32 @@
+package javacore.ZZDoptional.test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalTest01 {
+    public static void main(String[] args) {
+        Optional<String> optional1 = Optional.of("Aha Uhu o DevDojo é foda");
+        Optional<String> optional2 = Optional.ofNullable(null);
+        Optional<String> optional3 = Optional.empty();
+
+        System.out.println(optional1);
+        System.out.println(optional2);
+        System.out.println(optional3);
+        System.out.println("-".repeat(20));
+
+        Optional<String> nameOptional = findName("William");
+        String empty = nameOptional.orElse("EMPTY");
+        System.out.println(empty);
+
+        nameOptional.ifPresent(s -> System.out.println(s.toUpperCase()));
+    }
+
+    private static Optional<String> findName(String name) {
+        List<String> list = List.of("William", "DevDojo");
+        int i = list.indexOf(name);
+        if (i != -1) {
+            return Optional.of(list.get(i));
+        }
+        return Optional.empty();
+    }
+}
